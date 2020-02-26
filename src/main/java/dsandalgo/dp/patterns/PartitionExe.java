c3bd77c47de224fb9a98004867b5ff90d9e416fa
marshalls <mshi@whiteclarkegroup.com>
2020-02-26 15:26:34 +1100

    add new questions.

--- src/main/java/dsandalgo/dp/patterns/PartitionExe.java
+++ src/main/java/dsandalgo/dp/patterns/PartitionExe.java
@@ -7,8 +7,57 @@ public class PartitionExe {
 
 public static void main(String[] args) {
         PartitionExe exe = new PartitionExe();
-        int[] iput = {1,3,1,4,1,5};
-        System.out.println(exe.minScoreTriangulation(iput));
+        int[] iput = {7,2,5,10,8};
+        System.out.println(exe.splitArray(iput, 2));
+    }
+
+
+    /**
+     * https://leetcode.com/problems/split-array-largest-sum/
+     * Given an array which consists of non-negative integers and an integer m, you can split the array into m non-empty continuous subarrays.
+     * Write an algorithm to minimize the largest sum among these m subarrays.
+     *
+     * Note:
+     * If n is the length of array, assume the following constraints are satisfied:
+     *
+     * 1 ≤ n ≤ 1000
+     * 1 ≤ m ≤ min(50, n)
+     * Examples:
+     *
+     * Input:
+     * nums = [7,2,5,10,8]
+     * m = 2
+     *
+     * Output:
+     * 18
+     *
+     * Explanation:
+     * There are four ways to split nums into two subarrays.
+     * The best way is to split it into [7,2,5] and [10,8],
+     * where the largest sum among the two subarrays is only 18.
+     */
+    public int splitArray(int[] nums, int m) {
+        int[] sum = new int[nums.length];
+        for (int i = 0;i < nums.length; i++) {
+            sum[i] = nums[i] + (i > 0 ? sum[i-1] : 0);
+        }
+        int[][] cache = new int[1001][51];
+        return splitArrayHelper(nums, m, sum, nums.length, 0, cache);
+    }
+
+    public int splitArrayHelper(int[] A, int k, int[] sum, int len, int s, int[][] cache) {
+        if (cache[s][k] != 0) {
+            return cache[s][k];
+        }
+        if (k == 1) {
+            return (sum[len-1] - sum[s] + A[s]);
+        }
+        int num = Integer.MAX_VALUE;
+        for (int i = s; i + k <= len ; i++) {
+            num = Math.min(num, Math.max(sum[i] - sum[s] + A[s], splitArrayHelper(A, k-1, sum, len, i+1, cache)));
+        }
+        cache[s][k] = num;
+        return num;
     }
 
     /**
